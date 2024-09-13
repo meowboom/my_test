034be738c7edfa89846a8f75990a896ce79f102a
Maksim Dzinisiuk <101088991+meowboom@users.noreply.github.com>
2024-09-13 22:56:23 +0200

    sidebar

--- src/components/siderbar/SideBar.tsx
+++ src/components/siderbar/SideBar.tsx
@@ -19,21 +19,55 @@ import {
 import React, { FC, useEffect, useState } from "react";
 import SocialMedia from "../ui/socialmedia";
 
-interface ISideBar {}
+const svgImgComponent = {
+  section1: [
+    { title: "Swap", svg: <ArrowRightLeft className="size-7" /> },
+    { title: "Add liquidity", svg: <CirclePlus className="size-7" /> },
+    { title: "Manage liquidity", svg: <Waypoints className="size-7" /> },
+    { title: "Bitcoin Stake", svg: <Zap className="size-7" /> },
+    { title: "Pending liquidity", svg: <Clock className="size-7" /> },
+  ],
+  section2: [
+    { title: "Vesting", svg: <ChartPie className="size-7" /> },
+    { title: "Your Wallet", svg: <Wallet className="size-7" /> },
+    { title: "Thorned", svg: <CloudLightning className="size-7" /> },
+    { title: "Stats", svg: <ChartCandlestick className="size-7" /> },
+  ],
+};
 
-const SideBar: FC<ISideBar> = () => {
-  const [isOpen, setIsOpen] = useState(false);
-  const [isVisible, setIsVisible] = useState(false);
+const SideBar = () => {
+  const [isOpen, setIsOpen] = useState<boolean>(false);
+  const [isVisible, setIsVisible] = useState<boolean>(false);
+  const [isMenuFull, setIsMenuFull] = useState<boolean>(false);
+
   useEffect(() => {
     if (isOpen) return setIsVisible(false);
   }, [isOpen]);
-  console.log(isVisible);
+
+  useEffect(() => {
+    let timer: number;
+
+    if (isOpen) {
+      timer = window.setTimeout(() => {
+        setIsMenuFull(true);
+      }, 120);
+    } else {
+      setIsMenuFull(false);
+    }
+    return () => {
+      clearTimeout(timer);
+    };
+  }, [isOpen]);
 
   return (
-    <div className="fixed left-10 top-1/2 transform -translate-y-1/2 h-3/4 bg-primaryBg text-primaryText text-xl rounded-3xl flex flex-col gap-4 justify-between px-6 py-12">
+    <div
+      className={`${
+        isOpen ? "w-72" : "w-28"
+      } transition-all z-[9999] fixed left-10 top-1/2 -translate-y-1/2 h-3/4 bg-primaryBg text-primaryText text-xl rounded-3xl flex flex-col gap-4 justify-between px-6 py-12`}
+    >
       <section className="w-full flex gap-4 items-center justify-center">
         <Bitcoin className="size-11 border border-[#e47248] p-1 text-[#e47248] rounded-full" />
-        {isOpen && <h2 className="text-2xl">Bitcoin</h2>}
+        {isMenuFull && <h2 className="text-2xl">Bitcoin</h2>}
       </section>
       <section className="flex flex-col justify-start mt-10 mb-5">
         <div
@@ -48,84 +82,34 @@ const SideBar: FC<ISideBar> = () => {
                 : "hover:after:content-['Dashboard'] after:absolute after:-bottom-4 after:left-1/2 after:-translate-x-1/2"
             }`}
           />
-          {isOpen && <h4>Dashboard</h4>}
+          {isMenuFull && <h4>Dashboard</h4>}
         </div>
         <section className="py-2 pl-2 pr-3 rounded-2xl bg-secondaryGray/30 flex flex-col justify-center items-start gap-2">
-          <div
-            className={`${
-              isOpen ? "justify-start" : "justify-center"
-            } w-full duration-300 ease-in-out cursor-pointer flex gap-4  items-center hover:bg-primaryGreen hover:text-secondaryText rounded-xl py-1 px-2`}
-          >
-            <ArrowRightLeft className="size-7" />
-            {isOpen && <h4>Swap</h4>}
-          </div>
-          <div
-            className={`${
-              isOpen ? "justify-start" : "justify-center"
-            } w-full duration-300 ease-in-out cursor-pointer flex gap-4  items-center hover:bg-primaryGreen hover:text-secondaryText rounded-xl py-1 px-2`}
-          >
-            <CirclePlus className="size-7" />
-            {isOpen && <h4>Add liquidity</h4>}
-          </div>
-          <div
-            className={`${
-              isOpen ? "justify-start" : "justify-center"
-            } w-full duration-300 ease-in-out cursor-pointer flex gap-4  items-center hover:bg-primaryGreen hover:text-secondaryText rounded-xl py-1 px-2`}
-          >
-            <Waypoints className="size-7" />
-            {isOpen && <h4>Manage liquidity</h4>}
-          </div>
-          <div
-            className={`${
-              isOpen ? "justify-start" : "justify-center"
-            } w-full duration-300 ease-in-out cursor-pointer flex gap-4  items-center hover:bg-primaryGreen hover:text-secondaryText rounded-xl py-1 px-2`}
-          >
-            <Zap className="size-7" />
-            {isOpen && <h4>Bitcoin Stake</h4>}
-          </div>
-          <div
-            className={`${
-              isOpen ? "justify-start" : "justify-center"
-            } w-full duration-300 ease-in-out cursor-pointer flex gap-4  items-center hover:bg-primaryGreen hover:text-secondaryText rounded-xl py-1 px-2`}
-          >
-            <Clock className="size-7" />
-            {isOpen && <h4>Pending liquidity</h4>}
-          </div>
+          {svgImgComponent.section1.map(({ svg, title }) => (
+            <div
+              key={title}
+              className={`${
+                isOpen ? "justify-start" : "justify-center"
+              } w-full duration-300 ease-in-out cursor-pointer flex gap-4  items-center hover:bg-primaryGreen hover:text-secondaryText rounded-xl py-1 px-2`}
+            >
+              {svg}
+              {isMenuFull && <h4>{title}</h4>}
+            </div>
+          ))}
         </section>
       </section>
       <section className="mb-14 pl-2 pr-3 flex flex-col justify-center items-center gap-2 ">
-        <div
-          className={`${
-            isOpen ? "justify-start" : "justify-center"
-          } w-full duration-300 ease-in-out cursor-pointer flex gap-4  items-center hover:bg-primaryBlue hover:text-secondaryText rounded-xl py-1 px-2`}
-        >
-          <ChartPie className="size-7" />
-          {isOpen && <h4>Vesting</h4>}
-        </div>
-        <div
-          className={`${
-            isOpen ? "justify-start" : "justify-center"
-          } w-full duration-300 ease-in-out cursor-pointer flex gap-4  items-center hover:bg-primaryBlue hover:text-secondaryText rounded-xl py-1 px-2`}
-        >
-          <Wallet className="size-7" />
-          {isOpen && <h4>Your Wallet</h4>}
-        </div>
-        <div
-          className={`${
-            isOpen ? "justify-start" : "justify-center"
-          } w-full duration-300 ease-in-out cursor-pointer flex gap-4  items-center hover:bg-primaryBlue hover:text-secondaryText rounded-xl py-1 px-2`}
-        >
-          <CloudLightning className="size-7" />
-          {isOpen && <h4>Thorned</h4>}
-        </div>
-        <div
-          className={`${
-            isOpen ? "justify-start" : "justify-center"
-          } w-full duration-300 ease-in-out cursor-pointer flex gap-4  items-center hover:bg-primaryBlue hover:text-secondaryText rounded-xl py-1 px-2`}
-        >
-          <ChartCandlestick className="size-7" />
-          {isOpen && <h4>Stats</h4>}
-        </div>
+        {svgImgComponent.section2.map(({ svg, title }) => (
+          <div
+            key={title}
+            className={`${
+              isOpen ? "justify-start" : "justify-center"
+            } w-full duration-300 ease-in-out cursor-pointer flex gap-4  items-center hover:bg-primaryBlue hover:text-secondaryText rounded-xl py-1 px-2`}
+          >
+            {svg}
+            {isMenuFull && <h4>{title}</h4>}
+          </div>
+        ))}
         <div
           onClick={() => setIsOpen(!isOpen)}
           className={`${
@@ -137,12 +121,12 @@ const SideBar: FC<ISideBar> = () => {
           ) : (
             <PanelLeftOpen className="size-7" />
           )}
-          {isOpen && <h4>Collapse sidebar</h4>}
+          {isMenuFull && <h4>Collapse sidebar</h4>}
         </div>
       </section>
       <section className="flex justify-center items-center">
         <div className="flex justify-center items-center gap-3">
-          {isOpen ? (
+          {isMenuFull ? (
             <SocialMedia />
           ) : (
             <div>
@@ -150,15 +134,18 @@ const SideBar: FC<ISideBar> = () => {
                 onClick={() => setIsVisible(!isVisible)}
                 className="size-10 hover:text-secondaryText duration-300 ease-in-out cursor-pointer"
               />
-              {isVisible ? (
-                <div className="relative">
-                  <div className="absolute flex -top-12 left-16 bg-primaryBg p-2 pr-4 gap-2 rounded-r-2xl  ">
-                    <SocialMedia />
-                  </div>
+
+              <div className="relative">
+                <div
+                  className={`${
+                    isVisible
+                      ? "translate-x-16 opacity-100"
+                      : "translate-x-10 opacity-0"
+                  } ease-in-out transform transition-all duration-500 -top-12 absolute flex bg-primaryBg p-2 pr-4 gap-2 rounded-r-2xl`}
+                >
+                  <SocialMedia />
                 </div>
-              ) : (
-                ""
-              )}
+              </div>
             </div>
           )}
         </div>
